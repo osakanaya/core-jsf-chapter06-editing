3a8e19dbceea8f39bd5c1a00fe2299da9186b756
unknown <sashida@SBD751SASHIDA1.nature.local>
2013-05-10 16:44:26 +0900

    initial commit

---
+++ src/main/java/com/corejsf/TableData.java
@@ -0,0 +1,31 @@
+package com.corejsf;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+@Named
+@SessionScoped
+public class TableData implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private static final Name[] names = {
+	       new Name("William", "Dupont"),
+	       new Name("Anna", "Keeney"),
+	       new Name("Mariko", "Randor"),
+	       new Name("John", "Wilson")
+	};
+	
+	public Name[] getNames() {
+		return names;
+	}
+	
+	public String save() {
+		for(Name name : names) {
+			name.setEditable(false);
+		}
+		return null;
+	}
+}
